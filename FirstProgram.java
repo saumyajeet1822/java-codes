778219ff1c027ba19e7e15d4a4c3ecbaf62b8b8f
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2022-09-13 17:26:20 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+public class logical
+{
+	public static void main(String[] args)
+	{
+// example 1
+		int x = 3;
+		int y = 2;
+		int z = 5;
+		boolean ans = x>y && x>z;
+		System.out.println(ans);
+	}
+}
