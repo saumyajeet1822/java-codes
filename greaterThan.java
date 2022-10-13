b3e485b45190a6a6794138212fb6733845c35543
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2022-10-13 18:51:45 +0530

    Add files via upload

---
+++ greaterThan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class greaterThan {
+    public static int greaterValue(int a, int b){
+        if(a>b){
+            return a;
+        }else {
+            return b;
+        }
+    }
+    public static void main(String[]args){
+        System.out.println("Please enter the first value: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Please enter the Second value: ");
+        int b = sc.nextInt();
+
+        System.out.println("Greater Value: ");
+        System.out.println(greaterValue(a,b)) ;
+    }
+}
