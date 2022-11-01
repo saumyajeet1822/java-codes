b75d1e47624b02c48b2b3b91b8ce13d700cff1bf
Saumyajeet Das <111430070+saumyajeet1822@users.noreply.github.com>
2022-11-01 18:50:35 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern {
+    public static void main(String[] args) {
+        //outer loop
+        for(int line =1; line<=4;line++){
+            //inner loop
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
